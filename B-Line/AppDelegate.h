5cb44eb34e4aee70d9cf5114bd04083c566406bb
nihakue <walden.gabrielw@gmail.com>
2013-04-15 12:08:20 -0600

    Initial Commit

---
+++ B-Line/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B-Line
+//
+//  Created by Gabriel West on 4/15/13.
+//  Copyright (c) 2013 Gabriel West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ B-Line/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  B-Line
+//
+//  Created by Gabriel West on 4/15/13.
+//  Copyright (c) 2013 Gabriel West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
